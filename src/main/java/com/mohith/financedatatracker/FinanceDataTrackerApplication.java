e948b4b0571a7cb588d2b77960ac996001369130
mohit <s.mohithkumar1511@gmail.com>
2026-04-02 23:27:08 +0530

    Initial commit

---
+++ src/main/java/com/mohith/financedatatracker/FinanceDataTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mohith.financedatatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDataTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceDataTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohith/financedatatracker/FinanceDataTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohith.financedatatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDataTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
